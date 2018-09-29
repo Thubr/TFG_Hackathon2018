a034c4dbd46b31318c72b264a4d3a2b448f1333a
Thubr <arthur07021999@yahoo.com.br>
2018-09-29 10:16:26 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;
+    public float rotSpeed;
+
+
+    private Rigidbody2D rb2D;
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        bool fire = Input.GetButton("Jump");
+        bool clockwise = Input.GetKey("l");
+        bool antiClockwise = Input.GetKey("j");
+
+        if (clockwise)
+        {
+            transform.Rotate(new Vector3(0.0f, 0.0f, -1.0f) * rotSpeed);
+        }
+
+        if (antiClockwise)
+        {
+            transform.Rotate(new Vector3(0.0f, 0.0f, 1.0f) * rotSpeed);
+        }
+
+        if (fire)
+        {       
+            rb2D.AddForce(transform.right * -speed);
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+
+public class Warp : MonoBehaviour 
+{
+	public Transform Player;
+
+	Camera cam;
+	
+	
+	Vector3 screenBottomLeft;
+	Vector3 screenTopRight;
+
+	float screenWidth;
+	float screenHeight;
+	
+	Transform[] ghosts = new Transform[8];
+
+	void CreateGhostShips()
+	{
+		for(int i = 0; i < 8; i++)
+		{
+			ghosts[i] = Instantiate(Player);
+	
+		}
+	}
+
+	void Start () 
+	{
+		cam = Camera.main;
+		screenBottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, transform.position.z));
+		screenTopRight = cam.ViewportToWorldPoint(new Vector3(0, 0, transform.position.z));
+		screenWidth = screenTopRight.x - screenBottomLeft.x;
+		screenHeight = screenTopRight.y - screenBottomLeft.y;
+		CreateGhostShips();	
+	}
+	
+
+	void PositionGhostShips()
+	{
+		// All ghost positions will be relative to the ships (this) transform,
+		// so let's star with that.
+		Vector3 ghostPosition = transform.position;
+	
+		// We're positioning the ghosts clockwise behind the edges of the screen.
+		// Let's start with the far right.
+		ghostPosition.x = transform.position.x + screenWidth;
+		ghostPosition.y = transform.position.y;
+		ghosts[0].position = ghostPosition;
+	
+		// Bottom-right
+		ghostPosition.x = transform.position.x + screenWidth;
+		ghostPosition.y = transform.position.y - screenHeight;
+		ghosts[1].position = ghostPosition;
+	
+		// Bottom
+		ghostPosition.x = transform.position.x;
+		ghostPosition.y = transform.position.y - screenHeight;
+		ghosts[2].position = ghostPosition;
+	
+		// Bottom-left
+		ghostPosition.x = transform.position.x - screenWidth;
+		ghostPosition.y = transform.position.y - screenHeight;
+		ghosts[3].position = ghostPosition;
+	
+		// Left
+		ghostPosition.x = transform.position.x - screenWidth;
+		ghostPosition.y = transform.position.y;
+		ghosts[4].position = ghostPosition;
+	
+		// Top-left
+		ghostPosition.x = transform.position.x - screenWidth;
+		ghostPosition.y = transform.position.y + screenHeight;
+		ghosts[5].position = ghostPosition;
+	
+		// Top
+		ghostPosition.x = transform.position.x;
+		ghostPosition.y = transform.position.y + screenHeight;
+		ghosts[6].position = ghostPosition;
+	
+		// Top-right
+		ghostPosition.x = transform.position.x + screenWidth;
+		ghostPosition.y = transform.position.y + screenHeight;
+		ghosts[7].position = ghostPosition;
+	
+		/*// All ghost ships should have the same rotation as the main ship
+		for(int i = 0; i < 8; i++)
+		{
+			ghosts[i].rotation = transform.rotation;
+		}*/
+	}
+
+	void SwapShips()
+	{
+		foreach(var ghost in ghosts)
+		{
+			if (ghost.position.x < screenWidth && ghost.position.x > -screenWidth &&
+				ghost.position.y < screenHeight && ghost.position.y > -screenHeight)
+			{
+				transform.position = ghost.position;
+	
+				break;
+			}
+		}
+	
+		PositionGhostShips();
+	}
+
+	void Update () {
+		SwapShips();
+	}
+}
