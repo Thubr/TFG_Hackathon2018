89fdd027d6b413e0deceb2dc45ae82b71c277f10
Thubr <arthur07021999@yahoo.com.br>
2018-09-29 12:31:42 -0300

    Block prefab
    Block prefab includes a block basic attributes: maxLife, life, Transform, attached, colliders and rigidbody.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+	public int maxLife;
+	int life;
+	bool attached;
+	// Use this for initialization
+	void Start () {
+		life = maxLife;
+		attached = false;
+	}
+
+	public int Life{
+		get{
+			return life;
+		}
+	}
+
+	public bool Attached {
+		get{
+			return attached;
+		}
+		set{
+			attached = value;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
